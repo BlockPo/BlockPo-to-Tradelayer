4b4b2c82478e98ddfde3dd48ebde0617bccb0e60
santos177 <santos177@gmail.com>
2018-05-12 20:24:09 -0300

    adding changes in tests

--- src/test/omnilite2.sh
+++ src/test/omnilite2.sh
@@ -1,11 +1,11 @@
 SRC=/home/ale/Desktop/omni-lite/src
 DATADIR=/home/ale/.litecoin
 CONT=2147483651
-COL=2147483652
+COL=3
 PEGGED=2147483653
 AMOUNT=1000
 litecoins=5
-par1="Future Contract 1"
+par1="LTC/ALL"
 par2=20 # blocks until expiration
 par3=10 # Notional Size
 par5=2 #Margin Requirement
@@ -27,18 +27,9 @@ JSON="{\""${ADDR}"\":${litecoins},\""${ADDR2}"\":${litecoins},\""${ADDR3}"\":${l
 #Sending litecoins to all
 $SRC/litecoin-cli -datadir=$DATADIR --regtest sendmany "" $JSON > /dev/null
 $SRC/litecoin-cli -datadir=$DATADIR --regtest  generate 1 > /dev/null
-printf "Testing create payload rpc for createcontract: \n"
-$SRC/litecoin-cli -datadir=$DATADIR --regtest omni_createpayload_createcontract  2 2 "Derivaties" "Futures Contracts" "${par1}" 2 ${par2} ${par3} ${COL} ${par5}
-printf "Testing rpc for createcontract: \n"
-TRA=$($SRC/litecoin-cli -datadir=$DATADIR --regtest omni_createcontract ${ADDR} 2 2 "Derivatives" "Futures Contracts" "$par1" 2 ${par2} ${par3} ${COL} ${par5})
-$SRC/litecoin-cli -datadir=$DATADIR --regtest generate 1 >/dev/null
-$SRC/litecoin-cli -datadir=$DATADIR --regtest omni_gettransaction ${TRA}
-$SRC/litecoin-cli -datadir=$DATADIR --regtest  omni_listproperties
-printf "Testing create payload rpc for tradecontract: \n"
-$SRC/litecoin-cli -datadir=$DATADIR --regtest omni_createpayload_contract_trade 1 100 20 1
 # # Creating Divisible tokens (ALLs)
 printf "Creating ALLs:\n"   # TODO: see the indivisible/divisible troubles (amountToReserve < nBalance) in logicMath_Contra$
-$SRC/litecoin-cli -datadir=$DATADIR --regtest omni_sendissuancemanaged ${ADDR} 2 2 0 "All" "All" "All"
+$SRC/litecoin-cli -datadir=$DATADIR --regtest omni_sendissuancemanaged ${ADDR} 1 2 0 "All" "All" "All"
 $SRC/litecoin-cli -datadir=$DATADIR --regtest generate 1 >/dev/null
 $SRC/litecoin-cli -datadir=$DATADIR --regtest  omni_listproperties
 
@@ -53,30 +44,41 @@ printf "Checking ALL balance of ADDR2:\n"
 $SRC/litecoin-cli -datadir=$DATADIR --regtest  omni_getbalance ${ADDR2} ${COL}
 printf "Checking ALL balance of ADDR3:\n"
 $SRC/litecoin-cli -datadir=$DATADIR --regtest  omni_getbalance ${ADDR3} ${COL}
-printf "Testing rpc for trade contract: \n"
-TRA1=$($SRC/litecoin-cli -datadir=$DATADIR --regtest omni_tradecontract ${ADDR} ${CONT} 200 100 1)
+
+printf "Testing create payload rpc for createcontract: \n"
+$SRC/litecoin-cli -datadir=$DATADIR --regtest omni_createpayload_createcontract  1 2 "Futures Contracts" "${par1}" ${par2} ${par3} ${COL} ${par5}
+printf "Testing rpc for createcontract: \n"
+TRA=$($SRC/litecoin-cli -datadir=$DATADIR --regtest omni_createcontract ${ADDR} 1 2  "Futures Contracts" "$par1" ${par2} ${par3} ${COL} ${par5})
 $SRC/litecoin-cli -datadir=$DATADIR --regtest generate 1 >/dev/null
-$SRC/litecoin-cli -datadir=$DATADIR --regtest omni_gettransaction ${TRA1}
-TRA2=$($SRC/litecoin-cli -datadir=$DATADIR --regtest omni_tradecontract ${ADDR2} ${CONT} 100 100 2)
-$SRC/litecoin-cli -datadir=$DATADIR --regtest generate 1 >/dev/null
-$SRC/litecoin-cli -datadir=$DATADIR --regtest omni_gettransaction ${TRA2}
-printf "Trading more contracts: \n"
-$SRC/litecoin-cli -datadir=$DATADIR --regtest omni_tradecontract ${ADDR} ${CONT} 300 120 2
-$SRC/litecoin-cli -datadir=$DATADIR --regtest generate 1 >/dev/null
-$SRC/litecoin-cli -datadir=$DATADIR --regtest omni_tradecontract ${ADDR3} ${CONT} 200 120 1
-$SRC/litecoin-cli -datadir=$DATADIR --regtest generate 1 >/dev/null
-printf "Checking the buy orderbook : \n"
-$SRC/litecoin-cli -datadir=$DATADIR --regtest omni_getcontract_orderbook ${CONT} 1
-printf "Checking the sell orderbook : \n"
-$SRC/litecoin-cli -datadir=$DATADIR --regtest omni_getcontract_orderbook ${CONT} 2
-printf "Checking the position of ADDR : \n"
-$SRC/litecoin-cli -datadir=$DATADIR --regtest omni_getposition ${ADDR} ${CONT}
-printf "Checking the position of ADDR2 : \n"
-$SRC/litecoin-cli -datadir=$DATADIR --regtest omni_getposition ${ADDR2} ${CONT}
-printf "Checking ALL balance of ADDR : \n"
-$SRC/litecoin-cli -datadir=$DATADIR --regtest omni_getbalance ${ADDR} ${COL}
-printf "Checking ALL balance of ADDR2 : \n"
-$SRC/litecoin-cli -datadir=$DATADIR --regtest omni_getbalance ${ADDR2} ${COL}
-printf "Checking tradehistory rpc : \n"
-$SRC/litecoin-cli -datadir=$DATADIR --regtest omni_gettradehistory ${CONT}
+$SRC/litecoin-cli -datadir=$DATADIR --regtest omni_gettransaction ${TRA}
+$SRC/litecoin-cli -datadir=$DATADIR --regtest  omni_listproperties 1
+#$SRC/litecoin-cli -datadir=/home/daniel/testlitecoin omni_createcontract "mhcshYJKykPrxtnpxcqbfKEJKzVFJxQ6TV" 1 2  "Futures Contracts" "LTC/ALL" 20000 10 3 2
+#
+#
+# printf "Testing rpc for trade contract: \n"
+# TRA1=$($SRC/litecoin-cli -datadir=$DATADIR --regtest omni_tradecontract ${ADDR} ${CONT} 200 100 1)
+# $SRC/litecoin-cli -datadir=$DATADIR --regtest generate 1 >/dev/null
+# $SRC/litecoin-cli -datadir=$DATADIR --regtest omni_gettransaction ${TRA1}
+# TRA2=$($SRC/litecoin-cli -datadir=$DATADIR --regtest omni_tradecontract ${ADDR2} ${CONT} 100 100 2)
+# $SRC/litecoin-cli -datadir=$DATADIR --regtest generate 1 >/dev/null
+# $SRC/litecoin-cli -datadir=$DATADIR --regtest omni_gettransaction ${TRA2}
+# printf "Trading more contracts: \n"
+# $SRC/litecoin-cli -datadir=$DATADIR --regtest omni_tradecontract ${ADDR} ${CONT} 300 120 2
+# $SRC/litecoin-cli -datadir=$DATADIR --regtest generate 1 >/dev/null
+# $SRC/litecoin-cli -datadir=$DATADIR --regtest omni_tradecontract ${ADDR3} ${CONT} 200 120 1
+# $SRC/litecoin-cli -datadir=$DATADIR --regtest generate 1 >/dev/null
+# printf "Checking the buy orderbook : \n"
+# $SRC/litecoin-cli -datadir=$DATADIR --regtest omni_getcontract_orderbook ${CONT} 1
+# printf "Checking the sell orderbook : \n"
+# $SRC/litecoin-cli -datadir=$DATADIR --regtest omni_getcontract_orderbook ${CONT} 2
+# printf "Checking the position of ADDR : \n"
+# $SRC/litecoin-cli -datadir=$DATADIR --regtest omni_getposition ${ADDR} ${CONT}
+# printf "Checking the position of ADDR2 : \n"
+# $SRC/litecoin-cli -datadir=$DATADIR --regtest omni_getposition ${ADDR2} ${CONT}
+# printf "Checking ALL balance of ADDR : \n"
+# $SRC/litecoin-cli -datadir=$DATADIR --regtest omni_getbalance ${ADDR} ${COL}
+# printf "Checking ALL balance of ADDR2 : \n"
+# $SRC/litecoin-cli -datadir=$DATADIR --regtest omni_getbalance ${ADDR2} ${COL}
+# printf "Checking tradehistory rpc : \n"
+# $SRC/litecoin-cli -datadir=$DATADIR --regtest omni_gettradehistory ${CONT}
 $SRC/litecoin-cli --regtest --datadir=$DATADIR stop
