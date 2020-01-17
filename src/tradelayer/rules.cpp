6f71b095b0f18010bf1994d4dc285c140a41c1b6
santos177 <santos177@gmail.com>
2020-01-17 15:31:09 -0300

    some changes in admin address for vesting

--- src/tradelayer/rules.cpp
+++ src/tradelayer/rules.cpp
@@ -169,7 +169,7 @@ CTestNetConsensusParams::CTestNetConsensusParams()
     MSC_MANUALSP_BLOCK = 0;
     MSC_SEND_ALL_BLOCK = 0;
     MSC_CONTRACTDEX_BLOCK = 0;
-    MSC_VESTING_BLOCK = 0;
+    MSC_VESTING_BLOCK = 1334800;
     MSC_NODE_REWARD = 777;
 }
 
