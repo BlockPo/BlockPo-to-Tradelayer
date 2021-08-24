5b816a403f52ccab5fcdc6f9e7893afc6633328b
ale <santos177@gmail.com>
2021-08-24 09:47:06 -0400

    small fix

--- src/tradelayer/mdex.cpp
+++ src/tradelayer/mdex.cpp
@@ -247,8 +247,10 @@ void mastercore::x_TradeBidirectional(typename cd_PricesMap::iterator &it_fwdPri
 
           const int64_t amountReserved = getMPbalance(pnew->getAddr(), collateral, CONTRACTDEX_RESERVE);
 
-          assert(update_tally_map(pnew->getAddr(), collateral, amountReserved, BALANCE));
-          assert(update_tally_map(pnew->getAddr(), collateral,  -amountReserved, CONTRACTDEX_RESERVE));
+          if (0 < amountReserved) {
+              assert(update_tally_map(pnew->getAddr(), collateral, amountReserved, BALANCE));
+              assert(update_tally_map(pnew->getAddr(), collateral,  -amountReserved, CONTRACTDEX_RESERVE));
+          }
 
           pnew->setAmountForsale(0, "no_remaining");
           return;
